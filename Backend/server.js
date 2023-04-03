748b500bc148756ecc3bb15adcf2d8d7ef169e3b
Devanshi <devanshi3533.be20@chitkara.edu.in>
2023-04-03 23:38:14 +0530

    basic server, env configur, added dependenicies

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,18 @@
-// index.js
 const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const colors = require('colors');
+
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-// // middleware
-// app.use(bodyParser.json());
-// app.use(cors());
+app.get('/', (req, res) => res.send('Hello world!'));
 
-// // connect to MongoDB
-// mongoose.connect('mongodb://localhost:27017/hospital', { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => console.log('MongoDB connected'))
-//     .catch(err => console.log(err));
-
-// // define routes
-// const patientRoutes = require('./routes/patients');
-// app.use('/patients', patientRoutes);
-
-// const doctorRoutes = require('./routes/doctors');
-// app.use('/doctors', doctorRoutes);
-
-// const appointmentRoutes = require('./routes/appointments');
-// app.use('/appointments', appointmentRoutes);
-
-app.get("/", function(req,res){
-    res.send("running");
-})
-// start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
